41f5fad870a93224c08ab5dfdc52615d8fdbdb11
System Administrator <root@FPD011.local>
2019-02-22 15:04:58 +0100

    Remove horizontal bars on candle sticks & added a generative graph

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-p5.prototype.candleStick = function(x, y, w, h, min, max, candleColor, stickColor, secondStickColor) {
+p5.prototype.candleStick = function(x, y, w, h, min, max) {
 	// misc coords
 	let ltCornerX = x - w/2;
 	let ltCornerY = y - h/2;
@@ -24,32 +24,16 @@ p5.prototype.candleStick = function(x, y, w, h, min, max, candleColor, stickColo
 
 	let doStroke = this._renderer._doStroke;
 	let stroke = this._renderer._getStroke();
-
-	let doFill = this._renderer._doFill;
 	let fill = this._renderer._getFill();
 
 	// draw
-	if(typeof stickColor !== 'undefined') {
-		this.stroke(stickColor);
-	}
+	this.stroke(fill);
 	// - top vertical line
 	this.line(x, topBarY, x, ltCornerY);
-	// - top horizontal line
-	this.line(lineLeftX, topBarY, lineRightX, topBarY);
-
-	if(typeof secondStickColor !== 'undefined') {
-		this.stroke(secondStickColor);
-	}
 	// - bottom vertical line
 	this.line(x, rbCornerY, x, botBarY);
-	// - bottom horizontal line
-	this.line(lineLeftX, botBarY, lineRightX, botBarY);
-
-	if(typeof candleColor !== 'undefined') {
-		this.fill(candleColor);
-	}
-
 	this.noStroke();
+
 	this.rect(ltCornerX, ltCornerY, w, h);
 
 
@@ -60,12 +44,6 @@ p5.prototype.candleStick = function(x, y, w, h, min, max, candleColor, stickColo
 		this.noStroke();
 	}
 
-	if(doFill) {
-		this.fill(fill);
-	} else {
-		this.noFill();
-	}
-
 	this.rectMode(rectMode);
 }
 
@@ -74,17 +52,46 @@ let s = function( p ) {
   p.setup = function() {
     p.createCanvas(window.innerWidth, window.innerHeight);
 
-    console.log(this);
-    console.log(p._renderer._getFill());
+    p.background(200);
+    p.strokeWeight(4);
+    p.strokeCap(p.PROJECT);
+
+    let candleSize = 30;
+    let margin = 20;
+    let stepX = candleSize + margin;
+
+    let candleCount = Math.round(p.width / stepX);
+
+    let variability = .01;
+
+    let lastY = p.height;
+	let y = p.height / 2;
+	let totHeight = p.randomGaussian(.5,1) * (p.height / 12);
+	let min = p.randomGaussian(.5,1) * (totHeight / 4);
+	let max = p.randomGaussian(.5,1) * (totHeight / 4);;
+	let height = totHeight - min - max;
+    for (var i = 0; i < candleCount; i++) {
+    	// candleCount
+
+    	p.candleStick(margin + i*stepX, y, candleSize, height, min, max);
+
+    	lastY = y;
+		y = y + p.noise(i * variability) * totHeight;
+		if(y < lastY) {
+			p.fill(100,255,100);
+		} else {
+			p.fill(255,100,100);
+		}
+		totHeight = p.randomGaussian(.5,1) * (p.height / 12);
+		min = p.randomGaussian(.5,1) * (totHeight / 4);
+		max = p.randomGaussian(.5,1) * (totHeight / 4);;
+		height = totHeight - min - max;
+    }
+
+    // p.candleStick(100, 100, 40, 80, 20, 40, p.color(255), p.color(255,100,100), p.color(100,100,255));
   }
 
   p.draw = function() {
-    p.background(0);
-
-    p.fill(255);
-    p.stroke(100, 255, 100);
-    p.strokeWeight(10);
-    p.candleStick(100, 100, 40, 80, 20, 40, p.color(255), p.color(255,100,100), p.color(100,100,255));
   }
 }
 
